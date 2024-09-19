85cd871d101326d272eddaf5415f11d8886b989f
Linda Wang <48342363+LindaWang7@users.noreply.github.com>
2024-09-19 12:59:28 -0400

    fixed animation bug

--- React_UI/mirror_ui/src/components/CommentSection.js
+++ React_UI/mirror_ui/src/components/CommentSection.js
@@ -5,6 +5,7 @@ const CommentSection = ({ commentsData }) => {
   const [allComments, setAllComments] = useState([]); // All comments data
   const [visibleComments, setVisibleComments] = useState([]); // 3 visible comments
   const timer = useRef(null);
+  const container = useRef(null);
   const containerTop = 100;
   const commentH = 56;
 
@@ -27,76 +28,83 @@ const CommentSection = ({ commentsData }) => {
     setVisibleComments([...commentsArray]); // Show the first 3 comments initially
   }, [commentsData]);
 
-  const container = useCallback(
-    (node) => {
-      if (node !== null && allComments.length) {
-        let PositionTop = -(commentH * allComments.length) * 2;
+  // const container = useCallback(
+  //   (node) => {
+  //     if (node !== null && allComments.length) {
 
-        node.style.top = PositionTop + "px";
-        if (timer.current) {
-          clearInterval(timer.currents);
-        }
+  //     }
+  //   },
+  //   [allComments]
+  // );
 
-        function isInViewport(element) {
-          const rect = element.getBoundingClientRect();
-          return (
-            rect.top - containerTop >= -commentH &&
-            rect.bottom - containerTop <= 360 + commentH
-          );
-        }
+  useEffect(() => {
+    if (allComments.length && container.current) {
+      const node = container.current;
+      let PositionTop = -(commentH * allComments.length) * 2;
 
-        function setContent() {
+      node.style.top = PositionTop + "px";
+      if (timer.current) {
+        clearInterval(timer.currents);
+      }
+
+      function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+          rect.top - containerTop >= -commentH &&
+          rect.bottom - containerTop <= 360 + commentH
+        );
+      }
+
+      function setContent() {
+        let list = node.querySelectorAll("p");
+
+        list.forEach((mlNode, index) => {
+          const op =
+            ((360 + containerTop) / mlNode.getBoundingClientRect().bottom - 1) *
+            2;
+          const ob =
+            (mlNode.getBoundingClientRect().top + commentH) /
+              (commentH + containerTop) -
+            1;
+          if (ob < 1) {
+            mlNode.style.color = `rgba(255,255,255,${ob})`;
+          }
+          if (op < 0.8 && op > -0.15) {
+            mlNode.style.color = `rgba(255,255,255,${op})`;
+          }
+          if (!isInViewport(mlNode)) {
+            mlNode.style.color = `rgba(255,255,255,1)`;
+          }
+        });
+      }
+
+      setContent();
+
+      window.requestAnimationFrame(move);
+      function move() {
+        PositionTop = Number((PositionTop + .2).toFixed(2));
+        if (PositionTop >= -(commentH * allComments.length)) {
           let list = node.querySelectorAll("p");
-
+          const newElements = [];
           list.forEach((mlNode, index) => {
-            const op =
-              ((360 + containerTop) / mlNode.getBoundingClientRect().bottom -
-                1) *
-              2;
-            const ob =
-              (mlNode.getBoundingClientRect().top + commentH) /
-                (commentH + containerTop) -
-              1;
-            if (ob < 1) {
-              mlNode.style.color = `rgba(255,255,255,${ob})`;
-            }
-            if (op < 0.8 && op > -0.15) {
-              mlNode.style.color = `rgba(255,255,255,${op})`;
-            }
-            if (!isInViewport(mlNode)) {
-              mlNode.style.color = `rgba(255,255,255,1)`;
+            if (index > list.length - allComments.length - 1) {
+              newElements.push(mlNode);
+              mlNode.remove();
             }
           });
+          newElements.reverse().forEach(function (element) {
+            node.insertBefore(element, node.firstChild);
+          });
+          PositionTop = -(commentH * allComments.length) * 2;
         }
-
+        // setTop(PositionTop);
+        // console.log(PositionTop);
+        node.style.top = PositionTop + "px";
         setContent();
-        function move() {
-          PositionTop += 0.1;
-          if (PositionTop >= -(commentH * allComments.length)) {
-            let list = node.querySelectorAll("p");
-            console.log(list);
-            const newElements = [];
-            list.forEach((mlNode, index) => {
-              if (index > list.length - allComments.length - 1) {
-                newElements.push(mlNode);
-                mlNode.remove();
-              }
-            });
-            console.log(newElements);
-            newElements.forEach(function (element) {
-              node.insertBefore(element, node.firstChild);
-            });
-            PositionTop = -(commentH * allComments.length) * 2;
-          }
-          // setTop(PositionTop);
-          node.style.top = PositionTop + "px";
-          setContent();
-        }
-        timer.current = setInterval(move, 1);
       }
-    },
-    [allComments]
-  );
+      timer.current = setInterval(move, 10);
+    }
+  }, [allComments.length, container.current]);
 
   useEffect(() => {
     return () => {
